8401eaac942386f77b12e0cccfa074446c3b3e06
sidd0001 <preeteesh.kakkar@successivesoftwares.com>
2015-05-26 20:28:17 +0530

    Update page2helper.js

--- client/helpers/page2helper.js
+++ client/helpers/page2helper.js
@@ -1,18 +1,37 @@
 Meteor.subscribe("images");
+Meteor.subscribe("userList");
+Meteor.subscribe("sendimages");
+Meteor.subscribe("RecivedImages");
 Template.Page2.helpers({
-	images: function() {
-        var currentUserId = Meteor.userId();
+    images: function() {
         if (Session.get("text")) {
-            return ImageList.find({userID: currentUserId , hashTag : Session.get("text") });
+            return ImageList.find({userID: Meteor.userId() , hashTag : Session.get("text") });
         } else {
-            return ImageList.find({userID: currentUserId});
+            return ImageList.find({userID: Meteor.userId()});
         }
-
+    },
+    recivedImages : function () {
+        return SendImageList.find({reciver: Meteor.userId() , view : false } );
     }
 });
 Template.Page2.events({
     'click #search':function() {
         var $text = $('#textSearch').val();
         Session.set('text' , $text);
+    },
+    'click .btnSend':function(){
+        $('.modal')
+            .modal('setting', 'transition', 'horizontal flip')
+            .modal('setting', 'closable', false )
+            .modal('setting', 'closable', false )
+            .modal('show');
+        Session.set('imgId' ,event.target.value);
+    },
+    'click .reciveImg': function(){
+
+        //ImageList.update( { _id : this._id }, { $set: { view : true } });
+            SendImageList.remove(this._id);
+
     }
+
 })
